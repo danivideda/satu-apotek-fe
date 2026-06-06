18cf1b268cb961b5480c19e94fc6c5994fe17d25
Dani Ihza Farrosi <dani.videda@gmail.com>
2026-06-06 21:31:59 +0700

    refactor: linkOptions for NavLink

--- src/routes/dashboard/-components/NavBar.tsx
+++ src/routes/dashboard/-components/NavBar.tsx
@@ -1,38 +1,32 @@
-import {
-  Link,
-  type RegisteredRouter,
-  type ValidateLinkOptions,
-} from '@tanstack/react-router'
+import { Link, type ValidateLinkOptions } from '@tanstack/react-router'
 import type React from 'react'
 
 export function NavBar() {
   return (
     <nav className="flex-1 bg-gray-50 border border-solid">
-      <NavLink title="Profile" linkOptions={{ to: '/dashboard/profile' }} />
+      <NavLink label="Profile" linkOptions={{ to: '/dashboard/profile' }} />
       <NavLink
-        title="Pharmacies"
+        label="Pharmacies"
         linkOptions={{ to: '/dashboard/pharmacies' }}
       />
     </nav>
   )
 }
 
-interface NavLinkProps<
-  TRouter extends RegisteredRouter = RegisteredRouter,
-  TOptions = unknown,
-> {
-  title: string
-  linkOptions: ValidateLinkOptions<TRouter, TOptions>
-}
-
-function NavLink(props: NavLinkProps): React.ReactNode {
+function NavLink({
+  label,
+  linkOptions,
+}: {
+  label: string
+  linkOptions: ValidateLinkOptions
+}): React.ReactNode {
   return (
-    <Link {...props.linkOptions}>
+    <Link {...linkOptions}>
       {({ isActive }) => (
         <div
           className={`${isActive ? 'bg-red-200' : 'bg-blue-100'} p-2 w-full border border-solid`}
         >
-          {props.title}
+          {label}
         </div>
       )}
     </Link>
