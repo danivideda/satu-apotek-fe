e81d53e4c7ce3552b9438f714fb4604b71bbcb60
Dani Ihza Farrosi <dani.videda@gmail.com>
2026-07-08 11:49:22 +0700

    patch: add button to add apotek in /pharmacies

--- src/routes/_owner/dashboard/pharmacies.tsx
+++ src/routes/_owner/dashboard/pharmacies.tsx
@@ -17,7 +17,10 @@ function RouteComponent() {
       <h1 className="mt-4 text-4xl text-black">Apotek</h1>
       <div className="flex flex-row flex-1 gap-2 mt-4">
         <section className="flex flex-col justify-center flex-1 p-4 text-right text-3xl">
-          <span className='font-light'>Pilih Apotek yang anda kelola</span>
+          <span className="font-light">
+            Pilih <span className="text-green-600">Apotek</span> yang anda
+            kelola
+          </span>
         </section>
         <section className="flex flex-col p-4 flex-1">
           {data.map((pharmacy, index) => (
@@ -25,14 +28,23 @@ function RouteComponent() {
               key={pharmacy.app_id}
               className="flex flex-row gap-3 p-2 py-4 border-b border-gray-200"
             >
-              <div className='flex flex-col justify-center text-gray-800'><div>{index + 1}</div></div>
+              <div className="flex flex-col justify-center text-gray-800">
+                <div>{index + 1}</div>
+              </div>
               <div>
-                <div className='text-green-600 font-semibold'>{pharmacy.name}</div>
-                <div className='text-gray-500 tracking-tight font-light'>{pharmacy.address}</div>
-                <div className='text-sm text-gray-500 tracking-tight font-light'>{pharmacy.app_id}</div>
+                <div className="text-black font-semibold">{pharmacy.name}</div>
+                <div className="text-gray-500 tracking-tight font-light">
+                  {pharmacy.address}
+                </div>
+                <div className="text-sm text-gray-500 tracking-tight font-light">
+                  {pharmacy.app_id}
+                </div>
               </div>
             </div>
           ))}
+          <div className="flex flex-col justify-center h-14 border-b border-dashed border-gray-200">
+            <span className="text-center text-green-600">Tambah apotek</span>
+          </div>
         </section>
       </div>
     </div>
