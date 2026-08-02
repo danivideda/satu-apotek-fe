b4bc7ce1412da9713fbb17b913fc6511c1770c12
Dani Ihza Farrosi <dani.videda@gmail.com>
2026-08-02 11:18:00 +0700

    wip: get pharmacy landing details data at

--- src/routes/app/landing.tsx
+++ src/routes/app/landing.tsx
@@ -1,9 +1,12 @@
 import { authPharmacyCheck } from '#/lib/auth'
+import { fetchHelper } from '#/lib/fetch'
+import { queryOptions, useQuery } from '@tanstack/react-query'
 import { createFileRoute, isRedirect, redirect } from '@tanstack/react-router'
+import z from 'zod'
 
 export const Route = createFileRoute('/app/landing')({
   beforeLoad: async ({ context }) => {
-    console.log('From beforeLoad landing.route')
+    console.log('From beforeLoad app/landing')
     try {
       const response = await authPharmacyCheck(context.queryClient)
       if (!response.ok) {
@@ -29,9 +32,50 @@ export const Route = createFileRoute('/app/landing')({
       }
     }
   },
+  loader: async ({ context }) => {
+    context.queryClient.fetchQuery(pharmacyLandingQueryOptions)
+  },
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/app/landing"!</div>
+  const { data: response, status } = useQuery(pharmacyLandingQueryOptions)
+  if (status === 'pending') return <div>Pending...</div>
+  if (status === 'error') return <div>Something went wrong...</div>
+
+  const data = response.data
+  return (
+    <div className='container mx-auto'>
+      <div className="flex flex-col">
+        <div>Selamat datang di {data.name}</div>
+        <div>
+          <h1>Masuk sebagai</h1>
+          {data.users.map((user, index) => (
+            <div key={`${user.username}+${index}`}>
+              Username: {user.username}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
+
+const pharmacyLandingQueryOptions = queryOptions({
+  queryKey: ['pharmacy', 'landing'],
+  queryFn: async () => {
+    const response = await fetchHelper('/pharmacy/landing')
+    if (!response.ok)
+      throw new Error(`Bad response, status: ${response.status}`)
+
+    const data = PharmacyLandingResponseSchema.parse(await response.json())
+    return data
+  },
+})
+
+const PharmacyLandingResponseSchema = z.object({
+  data: z.object({
+    name: z.string(),
+    users: z.array(z.object({ username: z.string() })),
+  }),
+})
