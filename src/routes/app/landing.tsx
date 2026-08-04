31eae124077129eb2368ba5e5d4da24aeb354568
Dani Ihza Farrosi <dani.videda@gmail.com>
2026-08-04 13:29:40 +0700

    feat: show password field and user list on app landing page

--- src/routes/app/landing.tsx
+++ src/routes/app/landing.tsx
@@ -1,7 +1,17 @@
 import { authPharmacyCheck } from '#/lib/auth'
+import cn from '#/lib/cn'
 import { fetchHelper } from '#/lib/fetch'
 import { queryOptions, useQuery } from '@tanstack/react-query'
 import { createFileRoute, isRedirect, redirect } from '@tanstack/react-router'
+import {
+  useEffect,
+  useRef,
+  useState,
+  type Dispatch,
+  type ReactHTMLElement,
+  type SetStateAction,
+} from 'react'
+import { Fragment } from 'react/jsx-runtime'
 import z from 'zod'
 
 export const Route = createFileRoute('/app/landing')({
@@ -43,20 +53,105 @@ function RouteComponent() {
   if (status === 'pending') return <div>Pending...</div>
   if (status === 'error') return <div>Something went wrong...</div>
 
+  const [activeItemKey, setActiveItemKey] = useState('NoneSelected')
+  const isNoneSelected = activeItemKey === 'NoneSelected'
+  const [password, setPassword] = useState('')
+
+  useEffect(() => {
+    // reset password field when selecting different user
+    // runs when activeItemKey is changed AND an item is selected
+    if (!isNoneSelected) setPassword('')
+  }, [activeItemKey])
+
   const data = response.data
   return (
-    <div className='container mx-auto'>
-      <div className="flex flex-col">
-        <div>Selamat datang di {data.name}</div>
-        <div>
-          <h1>Masuk sebagai</h1>
-          {data.users.map((user, index) => (
-            <div key={`${user.username}+${index}`}>
-              Username: {user.username}
+    <div className="flex flex-row h-lvh">
+      <div className='flex-1 bg-green-100 opacity-45 bg-[url("/endless-clouds.svg")]'></div>
+      <div className="flex-1 bg-white container mx-auto p-4">
+        <div className="flex flex-col h-full justify-center">
+          <div className="">
+            <div className="text-lg">Selamat datang di</div>
+            <div className="text-4xl">{data.name}</div>
+          </div>
+          <div className="mt-4 flex flex-col gap-1.5">
+            <div>
+              <h1>Masuk sebagai</h1>
+              <div className="w-full flex flex-col gap-1.5">
+                {data.users.map((user, index) => {
+                  const itemKey = `${user.username}+${index}`
+                  const isActive = itemKey === activeItemKey
+                  const onClickCallback = () =>
+                    setActiveItemKey((prev) => {
+                      if (itemKey === prev) {
+                        return 'NoneSelected'
+                      }
+                      return itemKey
+                    })
+
+                  return (
+                    <UserLoginItemComponent
+                      key={itemKey}
+                      user={user}
+                      isActive={isActive}
+                      isNoneSelected={isNoneSelected}
+                      onClickCallback={onClickCallback}
+                    />
+                  )
+                })}
+              </div>
             </div>
-          ))}
+            <div
+              className={cn(
+                'flex flex-col gap-1.5 transition-all ease-in-out',
+                {
+                  'invisible opacity-0': isNoneSelected,
+                },
+              )}
+            >
+              <div>
+                <div>Password:</div>
+                <input
+                  type="password"
+                  placeholder="type password"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value)
+                  }}
+                />
+              </div>
+              <button className='p-2 border border-green-600 bg-green-100 cursor-pointer rounded-lg'>Login</button>
+            </div>
+          </div>
         </div>
       </div>
+    </div>
+  )
+}
+
+const UserLoginItemComponent = ({
+  user,
+  isActive,
+  isNoneSelected,
+  onClickCallback,
+}: {
+  user: { id: number; username: string }
+  isNoneSelected: boolean
+  isActive: boolean
+  onClickCallback: any
+}) => {
+  return (
+    <div
+      className={cn(
+        'border border-black p-4 rounded-2xl transition-all ease-in-out cursor-pointer',
+        {
+          'bg-gray-100 text-gray-500': !isNoneSelected,
+          'bg-green-300 text-black': isActive,
+          'hover:bg-green-50': !isActive,
+        },
+      )}
+      onClick={onClickCallback}
+    >
+      {user.username}
     </div>
   )
 }
@@ -76,6 +171,6 @@ const pharmacyLandingQueryOptions = queryOptions({
 const PharmacyLandingResponseSchema = z.object({
   data: z.object({
     name: z.string(),
-    users: z.array(z.object({ username: z.string() })),
+    users: z.array(z.object({ id: z.number(), username: z.string() })),
   }),
 })
